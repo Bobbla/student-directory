29cfdc82e2eda0c87b8d16bbd9f727c42f9c9672
Nick Roberts <nickrhys@me.com>
2014-08-05 21:43:44 +0100

    Added student count

--- directory.rb
+++ directory.rb
@@ -1,8 +1,12 @@
-puts "The Students of my cohort at Makers Academy"
-puts "--------------"
-puts "Javier Silverio"
-puts "Elliot Lewis"
-puts "Ben Tillett"
-puts "Vincent Koch"
-puts "Michelle Ballard"
-puts "Nick Roberts"
+puts ("The Students of my cohort at Makers Academy")
+puts ("--------------")
+puts ("Javier Silverio")
+puts ("Elliot Lewis")
+puts ("Ben Tillett")
+puts ("Vincent Koch")
+puts ("Michelle Ballard")
+puts ("Nick Roberts")
+
+print "Overall, we have "
+print 6
+puts " great students"
