2db89cb7244bed8c5a71634c727bab59cba670b0
Nick Roberts <nickrhys@me.com>
2014-08-05 21:47:00 +0100

    Comments added to code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first we print the list of students
 puts ("The Students of my cohort at Makers Academy")
 puts ("--------------")
 puts ("Javier Silverio")
@@ -6,7 +7,8 @@ puts ("Ben Tillett")
 puts ("Vincent Koch")
 puts ("Michelle Ballard")
 puts ("Nick Roberts")
-
+#finally, we print the total
 print "Overall, we have "
+#it's important that print () doesn't add new line characters
 print 6
 puts " great students"
