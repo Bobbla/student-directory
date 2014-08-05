b0268c0a96f9df94340bc95a85e7aa099e75ce97
Nick Roberts <nickrhys@me.com>
2014-08-05 22:06:25 +0100

    used arrays to simplify code

--- directory.rb
+++ directory.rb
@@ -1,13 +1,20 @@
-student_count = 9
+student_count = 6
 
-#first we print the list of students
-puts ("The Students of my cohort at Makers Academy")
+#let's put all students into an array
+students = [
+"Javier Silverio",
+"Elliot Lewis",
+"Ben Tillett",
+"Vincent Koch",
+"Michelle Ballard",
+"Nick Roberts",
+"Emma Feeney"
+]
+
+puts "The Students of my cohort at Makers Academy"
 puts ("--------------")
-puts ("Javier Silverio")
-puts ("Elliot Lewis")
-puts ("Ben Tillett")
-puts ("Vincent Koch")
-puts ("Michelle Ballard")
-puts ("Nick Roberts")
+students.each do |student|
+	puts student
+end
 #finally, we print the total
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.length} great students"
