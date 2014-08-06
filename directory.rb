e87b3139cbcbc34a7d94beabf9d85a61ac53c5b0
Nick Roberts <nickrhys@me.com>
2014-08-06 10:31:39 +0100

    changed puts to print

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	print "Please enter the names of the students\n"
+	print "To finish, just hit return twice\n"
 	#create an empty array
 	students = []
 	#get the first name
@@ -9,7 +9,7 @@ def input_students
 	while !name.empty? do
 		# add the student hash to the array
 		students << {:name => name, :cohort => :august}
-		puts "Now we have #{students.length} students"
+		print "Now we have #{students.length} students\n"
 		# get another name from user
 		name = gets.chomp
 	end
@@ -18,23 +18,22 @@ def input_students
 end
 
 def print_header
-	puts "The Students of my cohort at Makers Academy"
-	puts ("--------------")
+	print "The Students of my cohort at Makers Academy\n--------------\n"
 end
 
-def print(students)
+def print_student(students)
 	students.each do |student|
-		puts "#{student[:name]} (#{student[:cohort]} cohort)"
+		print "#{student[:name]} (#{student[:cohort]} cohort)\n"
 	end
 end
 
 def print_footer(students)
-	puts "Overall, we have #{students.length} great students"
+	print "Overall, we have #{students.length} great students\n"
 end
 #nothing happens until we call the methods
 students = input_students
 print_header
-print(students)
+print_student(students)
 print_footer(students)
 
 
